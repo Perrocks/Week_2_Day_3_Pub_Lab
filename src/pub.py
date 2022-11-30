dd0dcbdb823d5ec7fda48d96b03bf7552594fc8a
Liam King <liamking@live.co.uk>
2022-11-30 14:02:03 +0000

    added drinks to list

--- src/pub.py
+++ src/pub.py
@@ -2,4 +2,10 @@ class Pub:
     def __init__(self, name, till):
         self.name = name
         self.till = till
-        self.drinks = []
+        self.drinks_list = []
+
+    def drinks_list_length(self):
+        return len(self.drinks_list)
+
+    def add_to_drinks_list(self, drink_to_add):
+        self.drinks_list.append(drink_to_add)
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -1,10 +1,12 @@
 import unittest
 from src.pub import Pub
+from src.drink import Drink
 
 class TestPub(unittest.TestCase):
 
     def setUp(self):
         self.instance_of_Pub = Pub("The Drinking Scot", 500.00)
+        self.instance_of_Drink = Drink("Whiskey", 5.00)
 
     def test_pub_has_name(self):
         self.assertEqual("The Drinking Scot", self.instance_of_Pub.name)
@@ -12,4 +14,9 @@ class TestPub(unittest.TestCase):
     def test_pub_has_till(self):
         self.assertEqual(500.00, self.instance_of_Pub.till)
 
-    
+    def test_pub_has_drinks(self):
+        self.assertEqual(0, self.instance_of_Pub.drinks_list_length())
+
+    def test_can_add_drink_to_drinks_list(self):
+        self.instance_of_Pub.add_to_drinks_list(self.instance_of_Drink)
+        self.assertEqual(1, self.instance_of_Pub.drinks_list_length())
